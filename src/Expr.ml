488a533665e1209f9e53fb03b5893aef0cc367a0
kokhanov <kokhanov@yandex-team.ru>
2019-02-20 19:51:20 +0300

    hw1 done

--- src/Expr.ml
+++ src/Expr.ml
@@ -35,13 +35,18 @@ let update x v s = fun y -> if x = y then v else s y
 (* An example of a non-trivial state: *)                                                   
 let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
-(* Some testing; comment this definition out when submitting the solution. *)
+(* Some testing; comment this definition out when submitting the solution. 
 let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
+*)
+
+let to_bool e = e != 0
+
+let from_bool e = if e then 1 else 0
 
 (* Expression evaluator
 
@@ -50,5 +55,25 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let rec eval s expr = match expr with
+    | Const c -> c
+    | Var v -> s v
+    | Binop (op, left, right) -> 
+        let l = eval s left in
+        let r = eval s right in
+        match op with
+        | "+"  -> l + r
+        | "-"  -> l - r
+        | "*"  -> l * r
+        | "/"  -> l / r
+        | "%"  -> l mod r
+        | "<"  -> from_bool(l < r)
+        | "<=" -> from_bool(l <= r)
+        | ">"  -> from_bool(l > r)
+        | ">=" -> from_bool(l >= r)
+        | "==" -> from_bool(l = r)
+        | "!=" -> from_bool(l != r)
+        | "&&" -> from_bool(to_bool l && to_bool r)
+        | "!!" -> from_bool(to_bool l || to_bool r)
+        | _    -> failwith (Printf.sprintf "Unsupported operator %s" op)
+
